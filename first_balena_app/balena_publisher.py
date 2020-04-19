7f4bd98bbedbda3f7be139ded4ed78a09d3489ba
sansinghsanjay <san.singhsanjay@gmail.com>
2020-04-19 22:41:47 +0530

    added env var

--- first_balena_app/balena_publisher.py
+++ first_balena_app/balena_publisher.py
@@ -3,13 +3,13 @@ import random
 import time
 import os 
 import sys
-from PIL import Image
-from serial import SerialException
-import picamera
+#from PIL import Image
+#from serial import SerialException
+#import picamera
 
-print("* * * PIL installed and imported successfully * * *")
-print("* * * serial / SerialException installed and imported successfully * * *")
-print("* * * picamera imported successfully * * *")
+#print("* * * PIL installed and imported successfully * * *")
+#print("* * * serial / SerialException installed and imported successfully * * *")
+#print("* * * picamera imported successfully * * *")
 
 # importing the home variable
 sys.path.append("/first_balena_app/folder1")
@@ -24,8 +24,8 @@ folder2_function()
 print("* * * THIS IS THE FINAL RUN * * *")
 
 # camera module
-camera = picamera.PiCamera()
-camera.capture("/a.jpg")
+#camera = picamera.PiCamera()
+#camera.capture("/a.jpg")
 
 # create mqtt client object
 client = mqtt.Client()
@@ -36,11 +36,12 @@ client.connect("test.mosquitto.org", 1883, 60)
 # publish 30 random values after 2 second interval
 for i in range(30):
 	# open config file and read the value
-	file_ptr = open("./config.txt", "r")
-	data = file_ptr.read()
-	data = data.strip()
-	data = int(data.split(":")[1])
-	file_ptr.close()
+	#file_ptr = open("./config.txt", "r")
+	#data = file_ptr.read()
+	#data = data.strip()
+	#data = int(data.split(":")[1])
+	#file_ptr.close()
+	data = int(os.environ.get("normal_pressure"))
 	# get a random value within 0 and 100
 	random_pressure_value = random.randint(0, 100)
 	# publish this value
